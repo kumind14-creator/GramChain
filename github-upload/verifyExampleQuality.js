40383691dfb50bf3cf0ba3f97bb345ff517ebff0
kumind14-creator <kumind14@gmail.com>
2026-08-09 07:14:49 +0300

    Delete github-upload directory

--- github-upload/verifyExampleQuality.js
+++
@@ -1,7 +0,0 @@
-﻿const { auditExampleQuality } = require("./grammarEngine");
-
-const result = auditExampleQuality();
-console.log(JSON.stringify(result, null, 2));
-if (!result.ok) {
-  process.exitCode = 1;
-}
